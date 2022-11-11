74a2951c6b35a4424aca5fccbfd808595afd7cd3
mk-311 <mikojyo63@outlook.jp>
2022-11-11 15:57:12 +0900

    私の名前を付け足しました。

--- app/src/test/java/com/example/meirogame/ExampleUnitTest.java
+++ app/src/test/java/com/example/meirogame/ExampleUnitTest.java
@@ -14,5 +14,6 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
         System.out.println("萩原雅也");
+        System.out.println("三浦琴美");
     }
 }
